1c657a0a9d80fb80f4d5dfe0d36781bf188fdabf
YuelinWang <836473734@qq.com>
2020-08-02 23:10:03 +0800

    read readme

--- src/components/temSharelist.vue
+++ src/components/temSharelist.vue
@@ -3,26 +3,6 @@
     <el-row class="sharelistBox">
         <el-col :span="24" class="s-item tcommonBox">
             <header>
-                <h1>
-                    热门文章！
-                </h1>
-                <h2>
-                    <a href="https://mubu.com/doc/gvqRIFiMDP" target="_blank">首次进站请点此</a>
-                </h2>
-            </header>
-            <div class="article-content">
-                <el-row>
-                    <el-button type="primary" ><a href="https://mubu.com/doc/CAYihEYUS0" target="_blank" style="text-indent:15em;color:#FFF;" round="true">git教程</a></el-button>
-                    <el-button type="primary" ><a href="https://mubu.com/doc/a9YYeiV0Y0" target="_blank" style="text-indent:15em;color:#FFF;" round="true">h5教程</a></el-button>
-                    <el-button type="primary" ><a href="https://mubu.com/doc/1U8CWlk1GDP" target="_blank" style="text-indent:15em;color:#FFF;" round="true">c3教程</a></el-button>
-                    <el-button type="primary" ><a href="https://mubu.com/doc/xGLYYDX2g0" target="_blank" style="text-indent:15em;color:#FFF;" round="true">JS教程</a></el-button>
-                    <el-button type="primary" ><a href="https://mubu.com/doc/DMAXzFmpA3" target="_blank" style="text-indent:15em;color:#FFF;" round="true">前端学习路线</a></el-button>
-                </el-row>
-                <hr>
-            </div>
-        </el-col>
-        <el-col :span="24" class="s-item tcommonBox">
-            <header>
                 <h1>详细分类~</h1>
             </header>
             <br><br><br>
@@ -101,209 +81,36 @@
                     data:"2020年07月28日"
                 }],
                 biancheng:[{
-                    url:"http://html.wangyuelin.xyz/001.html",
-                    title:"端口可以用6666吗",
-                    data:"2020年01月01日"
+                    url:"https://mubu.com/doc/xGLYYDX2g0",
+                    title:"JavaScript教程",
+                    data:"2020年08月02日"
                 }],
                 suanfa:[{
-                    url:"http://html.wangyuelin.xyz/001.html",
-                    title:"端口可以用6666吗",
-                    data:"2020年01月01日"
+                    url:"https://mubu.com/doc/38BDb8R8imP",
+                    title:"八大算法",
+                    data:"2020年08月02日"
                 }],
                 mianjing:[{
-                    url:"http://html.wangyuelin.xyz/001.html",
+                    url:"http://html.wangyuelin.xyz/004.html",
                     title:"端口可以用6666吗",
                     data:"2020年01月01日"
                 }],
                 qita:[{
+                    url:"http://html.wangyuelin.xyz/005.html",
+                    title:"博客v1.0归档",
+                    data:"2020年08月02日"
+                },{
+                    url:"http://html.wangyuelin.xyz/003.html",
+                    title:"731记",
+                    data:"2020年07月31日"
+                },{
                     url:"http://html.wangyuelin.xyz/000.html",
                     title:"沈航新生问题汇总",
                     data:"2020年07月28日"
-                }],
-                articleList:[{
-                    url:"https://mubu.com/doc/6TBHXdZKfSP",
-                    title:"端口可以用6666吗",
-                    month: 7,
-                    day: 24,
-                    cate_name:"技术文章",
-                    description:"关于端口的一个小问题",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
                 },{
-                    url:"https://mubu.com/doc/38BDb8R8imP",
-                    title:"八大算法",
-                    month: 7,
-                    day: 24,
-                    cate_name:"技术文章",
-                    description:"过一遍常见算法",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/xGLYYDX2g0",
-                    title:"JavaScript教程",
-                    month: 7,
-                    day: 24,
-                    cate_name:"编程语言",
-                    description:"JS教程，来自廖雪峰，菜鸟等，还有一些自己的想法，这篇文章会长时间更新。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/M2HkG2WIOP",
-                    title:"vue权限路由实现方式总结",
-                    month: 7,
-                    day: 24,
-                    cate_name:"技术文章",
-                    description:"关于vue中权限是如何实现的",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/3RMtM8rDtIP",
-                    title:"js相等运算符与严格相等运算符",
-                    month: 7,
-                    day: 23,
-                    cate_name:"技术文章",
-                    description:"JS ==和===的区别",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/87cdLIYQmP",
-                    title:"JavaScript：彻底理解同步、异步和事件循环(Event Loop)",
-                    month: 7,
-                    day: 20,
-                    cate_name:"技术文章",
-                    description:"彻底理解同步、异步和事件循环",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/7OmXe0cL6WP",
-                    title:"Vue项目中优雅使用icon",
-                    month: 7,
-                    day: 20,
-                    cate_name:"技术文章",
-                    description:"关于icon的使用",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/6eLdvKlqZGP",
-                    title:"正则表达式",
-                    month: 7,
-                    day: 19,
-                    cate_name:"技术文章",
-                    description:"关于验证各种输入是否合法等",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/LSH4mOGfwP",
-                    title:"html5语义化标签",
-                    month: 7,
-                    day: 18,
-                    cate_name:"技术文章",
-                    description:"整理的一篇关于关于h5的文章，建议弄清楚html再来看",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/2qhaTAKnanP",
-                    title:"你真的了解回调吗",
-                    month: 7,
-                    day: 13,
-                    cate_name:"技术文章",
-                    description:"整理的一篇关于回调的文章",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/UT8EkrfHDP",
-                    title:"回调地狱",
-                    month: 7,
-                    day: 13,
-                    cate_name:"技术文章",
-                    description:"欢迎来到地狱~",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/wL_2wnAxTP",
-                    title:"沈阳航空航天大学新生须知",
-                    month: 7,
-                    day: 12,
-                    cate_name:"其他",
-                    description:"欢迎新生！",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/UT8EkrfHDP",
-                    title:"ES6详解",
-                    month: 7,
-                    day: 10,
-                    cate_name:"技术文章",
-                    description:"持续更新，真的太难了嘤嘤嘤",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/12i79Sq9hmP",
-                    title:"不会吧不会吧，不会现在还有人分不清 Cookie、Session、Token、JWT吧？",
-                    month: 7,
-                    day: 9,
-                    cate_name:"技术文章",
-                    description:"哥哥们别骂我，只是标题党，为了让你们看！",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/EBZCQzHZiP",
-                    title:"Vue教程",
-                    month: 7,
-                    day: 8,
-                    cate_name:"编程语言",
-                    description:"参考价值不高，全是vue官网的内容和自己做的笔记。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/2v1TQ8yLWyP",
-                    title:"2020——我的面经",
-                    month: 7,
-                    day: 8,
-                    cate_name:"面经",
-                    description:"是我太菜qaq。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/1y7fGqZhs5P",
-                    title:"算法路线",
-                    month: 7,
-                    day: 7,
-                    cate_name:"算法",
-                    description:"只是本菜鸡自己设计的路线，仅供小伙伴相互讨论。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/5kBpZFR5ShP",
-                    title:"前端——剑指offer",
-                    month: 7,
-                    day: 2,
-                    cate_name:"面经",
-                    description:"到处扒来的面经，可能有部分过时= =。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/DMAXzFmpA3",
-                    title:"前端路线",
-                    month: 6,
-                    day: 10,
-                    cate_name:"技术文章",
-                    description:"冲鸭前端！如果你有好的路线欢迎邮件我~",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/2iVFrMbSAOP",
-                    title:"原型与原型链",
-                    month: 6,
-                    day: 3,
-                    cate_name:"技术文章",
-                    description:"原型与原型链，需要一定基础才能看懂。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/2hp7ULPB8GP",
-                    title:"前端模块化",
-                    month: 5,
-                    day: 27,
-                    cate_name:"技术文章",
-                    description:"模块化理解，规范；CommonJS/AMD/CMD/ES6等。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/la6de5DHXP",
-                    title:"Webpack基本使用（详解）",
-                    month: 5,
-                    day: 20,
-                    cate_name:"技术文章",
-                    description:"关于webpack一些使用心得。",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
-                },{
-                    url:"https://mubu.com/doc/CAYihEYUS0",
-                    title:"不会吧不会吧，不会现在还有人不会用git吧？",
-                    month: 4,
-                    day: 20,
-                    cate_name:"技术文章",
-                    description:"哥哥们别骂我，只是标题党，为了让你们看！",
-                    image:"https://13miki.cdn.bcebos.com/menhera%2F",
+                    url:"http://html.wangyuelin.xyz/002.html",
+                    title:"717记",
+                    data:"2020年07月17日"
                 }]
             }
         },
